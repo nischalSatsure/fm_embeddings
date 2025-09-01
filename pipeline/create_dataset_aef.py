829972c3462d0f320fe1bf75c6d96be30cee9473
nischal_singh <nischal@satsure.co>
2025-09-01 12:34:01 +0530

    add: dataset configs

--- pipeline/create_dataset_aef.py
+++ pipeline/create_dataset_aef.py
@@ -9,37 +9,37 @@ import logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
 
 
-@hydra.main(version_base="1.3", config_path="../configs/data", config_name="default")
+@hydra.main(version_base="1.3", config_path="../configs", config_name="dataset")
 def main(cfg: DictConfig):
 
     max_workers = cfg.max_workers
-    filepath = cfg.aef.filepath
-    year = cfg.aef.year
-    output_path = cfg.aef.output_path
-    clip = cfg.aef.clip
-    cls = cfg.aef.cls
+    filepath = cfg.dataset.filepath
+    year = cfg.dataset.year
+    output_path = cfg.dataset.output_path
+    clip = cfg.dataset.clip
+    cls = cfg.dataset.cls
 
-    data_handler = AEFDataHandler()
-    if Path(output_path).suffix == '.parquet':
-        data_handler.create_dataset_from_polygons_parquet(
-            max_workers=max_workers,
-            filepath=filepath,
-            year=year,
-            output_path=output_path,
-            clip=clip,
-            cls=cls
-        )
-    else:
-        data_handler.create_dataset_from_polygons_csv(
-            max_workers=max_workers,
-            filepath=filepath,
-            year=year,
-            output_path=output_path,
-            clip=clip,
-            cls=cls
-        )
+    # data_handler = AEFDataHandler()
+    # if Path(output_path).suffix == '.parquet':
+    #     data_handler.create_dataset_from_polygons_parquet(
+    #         max_workers=max_workers,
+    #         filepath=filepath,
+    #         year=year,
+    #         output_path=output_path,
+    #         clip=clip,
+    #         cls=cls
+    #     )
+    # else:
+    #     data_handler.create_dataset_from_polygons_csv(
+    #         max_workers=max_workers,
+    #         filepath=filepath,
+    #         year=year,
+    #         output_path=output_path,
+    #         clip=clip,
+    #         cls=cls
+    #     )
 
-    logging.info(f"Dataset saved to {output_path}")
-
+    # logging.info(f"Dataset saved to {output_path}")
+    print(cfg)
 if __name__ == "__main__":
     main()
